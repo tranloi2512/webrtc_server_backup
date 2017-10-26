931376eb23ab6cfa396116512d1b54b5a28463f8
tranloi2512 <tranloi2512@gmail.com>
2017-10-26 22:12:55 +0700

    Add files via upload
    .

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 ///
 const socket = io('https://webrtcbk.herokuapp.com/');
 
+var speed, turn, target_speed, target_turn;
+var control_speed = 0; 
+var control_turn = 0;
+
 $('#div-chat').hide();
 
 ///
@@ -74,7 +78,7 @@ var stream_flag=false;
 peer.on('connection', function(conn) {
   // Receive messages
       conn.on('data', function(data) {
-      console.log('Received: ', data);
+      //console.log('Received: ', data);
       if (stream_flag==false)
       {
         const id = data;
@@ -89,21 +93,131 @@ peer.on('connection', function(conn) {
     });
 
         
-    }; //end of if
+    } //end of if
       
-     /* 
+     else {
+
+      speed = Number(document.getElementById('linearXText').value);
+      turn = Number(document.getElementById('angularZText').value)
       switch (data) {
-        case '#start':
-        console.log('start streaming');
-        const id =$(this).attr('id');
-        console.log('caller id:' +id);
+        case 73:  //forward
+        //console.log('move forward');  
+        target_speed = speed;
+        target_turn = 0;
         break;
-      }; //emd of switch*/
+        
+        case 188: //backward
+       // console.log('move backward');
+        target_speed = -speed;
+        target_turn = 0;
+        break;
+
+        case 74: //turn left
+        console.log('turn left');
+        target_speed = 0;
+        target_turn = turn;
+        break;
+
+        case 76: //turn right
+        console.log('turn right');
+        target_speed = 0;
+        target_turn = -turn;
+        break;
+
+        case 75: //stop
+        target_speed = 0;
+        target_turn = 0;
+        break;
+       
+        default: 
+        {
+          console.log('out of desired key '+data)
+        }; //end of default
+      }; //emd of switch
+      console.log('max'+Math.max(5,3));
+      if (target_speed > control_speed)
+                control_speed = Math.min( target_speed, control_speed + 0.02 );
+            else if (target_speed < control_speed)
+                control_speed = Math.max( target_speed, control_speed - 0.02 );
+            else
+                control_speed = target_speed;
+
+            if (target_turn > control_turn)
+                control_turn = Math.min( target_turn, control_turn + 0.1 );
+            else if (target_turn < control_turn)
+                control_turn = Math.max( target_turn, control_turn - 0.1 );
+            else
+                control_turn = target_turn;
+
+        twist.linear.x = control_speed;
+        twist.angular.z = control_turn;
+
+        // Publish the message 
+        cmdVelTopic.publish(twist);
+      //  console.log('target_speed = '+target_speed);
+      // console.log('target_turn = '+target_turn);
+
+
+    };  //end of else
      }); //end of data receive event
 }); //end of connection event
 
+//================================================================================================
+///
+///     Use ROSLIB for publishing cmd/Twist topic
+///
+// This function connects to the rosbridge server running on the local computer on port 9090
 
 
+var rbServer = new ROSLIB.Ros({
+    url : 'ws://localhost:9090'
+ });
+
+// This function is called upon the rosbridge connection event
+ rbServer.on('connection', function() {
+     // Write appropriate message to #feedback div when successfully connected to rosbridge
+     var fbDiv = document.getElementById('feedback');
+     fbDiv.innerHTML += "<p>Connected to websocket server.</p>";
+ });
+
+// This function is called when there is an error attempting to connect to rosbridge
+rbServer.on('error', function(error) {
+    // Write appropriate message to #feedback div upon error when attempting to connect to rosbridge
+    var fbDiv = document.getElementById('feedback');
+    fbDiv.innerHTML += "<p>Error connecting to websocket server.</p>";
+});
+
+// This function is called when the connection to rosbridge is closed
+rbServer.on('close', function() {
+    // Write appropriate message to #feedback div upon closing connection to rosbridge
+    var fbDiv = document.getElementById('feedback');
+    fbDiv.innerHTML += "<p>Connection to websocket server closed.</p>";
+ });
+
+// These lines create a topic object as defined by roslibjs
+var cmdVelTopic = new ROSLIB.Topic({
+    ros : rbServer,
+    name : '/cmd_vel_mux/input/teleop',
+    messageType : 'geometry_msgs/Twist'
+});
+
+// These lines create a message that conforms to the structure of the Twist defined in our ROS installation
+// It initalizes all properties to zero. They will be set to appropriate values before we publish this message.
+var twist = new ROSLIB.Message({
+    linear : {
+        x : 0.0,
+        y : 0.0,
+        z : 0.0
+    },
+    angular : {
+        x : 0.0,
+        y : 0.0,
+        z : 0.0
+    }
+});
+
+
+//================================================================================================
 
 ///
 ///     Window Closing Handler
@@ -162,6 +276,33 @@ function playStream(idVideoTag,stream){
 	const video = document.getElementById(idVideoTag);
 	video.srcObject = stream;
 	video.play();
+}
+
+
+/* This function:
+ - retrieves numeric values from the text boxes
+ - assigns these values to the appropriate values in the twist message
+ - publishes the message to the cmd_vel topic.
+ */
+function pubMessage() {
+    /**
+    Set the appropriate values on the twist message object according to values in text boxes
+    It seems that turtlesim only uses the x property of the linear object 
+    and the z property of the angular object
+    **/
+    var linearX = 0.0;
+    var angularZ = 0.0;
+
+    // get values from text input fields. Note for simplicity we are not validating.
+    linearX = 0 + Number(document.getElementById('linearXText').value);
+    angularZ = 0 + Number(document.getElementById('angularZText').value);
+
+    // Set the appropriate values on the message object
+    twist.linear.x = linearX;
+    twist.angular.z = angularZ;
+
+    // Publish the message 
+    cmdVelTopic.publish(twist);
 }
 
 
